ad4fe95db762d895c2c7b3bbdd198294e1b8edda
facuariasla <facundolautaroarias@hotmail.com>
2022-07-13 17:33:18 -0300

    working on addFavTokens - globalState

--- src/components/BuyPanel.jsx
+++ src/components/BuyPanel.jsx
@@ -1,100 +1,95 @@
-import React from 'react'
-import {  Stack, Text, FormControl, Input, InputGroup, Button, FormLabel, InputRightElement} from '@chakra-ui/react'
+import React, { useEffect } from "react";
+import {
+  Stack,
+  Text,
+  FormControl,
+  Input,
+  InputGroup,
+  Button,
+  FormLabel,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { FaChevronRight } from "react-icons/fa";
 
+import useStore from "../global_state";
+import BuyButtonList from "./BuyButtonList";
+
 const BuyPanel = () => {
+  const tokens = useStore((state) => state.tokens);
+  const fetchTokens = useStore((state) => state.fetchTokens);
+
+  useEffect(() => {
+    fetchTokens();
+  }, [fetchTokens]);
 
   const handleClick = () => {
-    console.log("Se muestra la lista");
+    console.log(tokens);
   };
 
   return (
     <Stack>
-    <Stack align="center">
-      <Text>BT PRICE $$$</Text>
+      <Stack align="center">
+        <Text>BT PRICE $$$</Text>
+      </Stack>
+
+      <form autoComplete="off">
+        <FormControl>
+          <Stack spacing={8}>
+            <Stack>
+              <FormLabel htmlFor="quantity" m={0}>
+                Cantidad
+              </FormLabel>
+
+              <Stack size="md" display="flex" direction='row' align='center'>
+                <Input
+                  id="quantity"
+                  type="text"
+                  h={16}
+                  placeholder="CONTIONAL CURRENCY"
+                  borderRadius={12}
+                  color="#fff"
+                  _placeholder={{ fontWeight: "400" }}
+                  fontWeight={500}
+                  bgColor='#493171'
+                  border='none'
+                />
+                  <BuyButtonList/>
+
+              </Stack>
+            </Stack>
+
+            <Stack>
+              <FormLabel htmlFor="quantity" m={0}>
+                Recibir
+              </FormLabel>
+
+              <Stack size="md" display="flex" direction='row' align='center'>
+                <Input
+                  id="quantity"
+                  type="text"
+                  h={16}
+                  placeholder="CONTIONAL CURRENCY"
+                  borderRadius={12}
+                  color="#fff"
+                  _placeholder={{ fontWeight: "400" }}
+                  fontWeight={500}
+                  bgColor='#493171'
+                  border='none'
+                />
+                  <BuyButtonList/>
+              </Stack>
+            </Stack>
+          </Stack>
+        </FormControl>
+
+        {/* ACA VA UN CONDITIONAL BUTTON */}
+      </form>
+
+
     </Stack>
-
-    <form autoComplete="off">
-      <FormControl>
-        <Stack spacing={8}>
+  );
+};
 
 
 
-          <Stack>
-            <FormLabel htmlFor="quantity" m={0}>
-              Cantidad
-            </FormLabel>
-            
-            <InputGroup size="md" display="flex">
-              <Input
-                id="quantity"
-                type="text"
-                h={16}
-                placeholder="CONTIONAL CURRENCY"
-                borderRadius={12}
-                color='#fff'
-                _placeholder={{fontWeight:'400'}}
-                fontWeight={500}
-              />
-              <InputRightElement width={['5.5rem', "8rem"]} h="100%">
-                <Button
-                  h="50%"
-                  w='80%'
-                  size="sm"
-                  onClick={handleClick}
-                  rightIcon={<FaChevronRight />}
-                  color='#2D0B5A'
-                  bgColor='#35F0D0'
-                  borderRadius={12}
-                >
-                  USDC
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </Stack>
-
-
-
-
-          <Stack>
-            <FormLabel htmlFor="quantity" m={0}>
-              Recibir
-            </FormLabel>
-
-            <InputGroup size="md" display="flex">
-              <Input
-                id="quantity"
-                type="text"
-                h={16}
-                placeholder="CONTIONAL CURRENCY"
-                borderRadius={12}
-                color='#fff'
-                _placeholder={{fontWeight:'400'}}
-                fontWeight={500}
-              />
-              <InputRightElement width={['5.5rem', "8rem"]} h="100%">
-                <Button
-                  h="50%"
-                  w='80%'
-                  size="sm"
-                  onClick={handleClick}
-                  rightIcon={<FaChevronRight />}
-                  color='#2D0B5A'
-                  bgColor='#35F0D0'
-                  borderRadius={12}
-                >
-                  BTC
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </Stack>
-        </Stack>
-      </FormControl>
-
-      {/* ACA VA UN CONDITIONAL BUTTON */}
-    </form>
-  </Stack>
-  )
-}
-
-export default BuyPanel
+export default BuyPanel;
