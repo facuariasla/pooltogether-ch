16c5b40960fa7fa91ec6359f3973129a65f4f6bc
facuariasla <facundolautaroarias@hotmail.com>
2022-07-14 16:12:43 -0300

    hash router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Buy from "./pages/Buy";
 import Home from "./pages/Home";
@@ -6,14 +6,12 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-
           <Route path="/" element={<Home />} />
           <Route path="/buy" element={<Buy />} />
-
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
